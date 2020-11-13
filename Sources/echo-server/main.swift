6d0f21814d75271506f724c7052f73a10d6f5cb6
ja.toctaguano <ja.toctaguano@alumnos.urjc.es>
2020-11-13 21:42:44 +0100

    mod

--- Sources/echo-server/main.swift
+++ Sources/echo-server/main.swift
@@ -1,5 +1,26 @@
 /* Server echo UDP.... */
+
 import Socket
+import Foundation
 
+let port = 7667
 
-let serverSocket = try Socket.create()
+do {
+    let serverSocket = try Socket.create(family: .inet, type: .datagram, proto: .udp)
+    try serverSocket.listen(on: port)
+    print("listening on \(port)")
+    var buffer = Data(capacity: 1000)
+    repeat {
+        let (bytesRead, clientAddress) = try serverSocket.readDatagram(into: &buffer)
+        if let address = clientAddress {
+            let (clientHostname, clientPort) = Socket.hostnameAndPort(from: address)!
+            print("Received lenght \(bytesRead) from \(clientHostname):\(clientPort)")
+            let message = String(decoding: buffer, as: UTF8.self)
+            print("Message: \(message)")
+        }
+        buffer.removeAll()
+    } while true
+} catch let error {
+    print("Connection error: \(error)")
+}
+    
