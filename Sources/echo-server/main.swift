54c00ced74ba859d3f0cf491ac70e675ead154ad
ja.toctaguano <ja.toctaguano@alumnos.urjc.es>
2020-11-15 20:06:57 +0100

    mods

--- Sources/echo-server/main.swift
+++ Sources/echo-server/main.swift
@@ -6,17 +6,27 @@ import Foundation
 do {
     let port = Int(CommandLine.arguments[1])!
 
+    // socket servidor
     let serverSocket = try Socket.create(family: .inet, type: .datagram, proto: .udp)
+    // escucha en un puerto especifico
     try serverSocket.listen(on: port)
     print("listening on \(port)")
+    // Buffer con determinada capacidad para almacenar datos
     var buffer = Data(capacity: 1000)
+
     repeat {
+        // extrae datos y direccion del ciente del datagrama 
         let (bytesRead, clientAddress) = try serverSocket.readDatagram(into: &buffer)
+        // si la direccion de los datos extraida es correcta
+        //print(clientAddress!)
         if let address = clientAddress {
+
             let (clientHostname, clientPort) = Socket.hostnameAndPort(from: address)!
             print("Received lenght \(bytesRead) from \(clientHostname):\(clientPort)")
             let message = String(decoding: buffer, as: UTF8.self)
-            print("Message: \(message)\n")
+            print("Client Message: \(message)\n")
+            let reply = "Server: \(message)"
+            try serverSocket.write(from: reply, to: address)//devuelve el mensaje al cliente
         }
         buffer.removeAll()
     } while true
