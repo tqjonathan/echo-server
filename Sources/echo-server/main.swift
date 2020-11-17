85820d0eded54c28730643313ec712a53ccd096f
ja.toctaguano <ja.toctaguano@alumnos.urjc.es>
2020-11-17 14:57:45 +0100

    .

--- Sources/echo-server/main.swift
+++ Sources/echo-server/main.swift
@@ -3,6 +3,13 @@
 import Socket
 import Foundation
 
+// let port = 7667
+
+// struct Point {
+//     var x: Double
+//     var y: Double
+// }
+
 do {
     let port = Int(CommandLine.arguments[1])!
 
@@ -18,12 +25,24 @@ do {
         // extrae datos y direccion del ciente del datagrama 
         let (bytesRead, clientAddress) = try serverSocket.readDatagram(into: &buffer)
         if let address = clientAddress {
-
             let (clientHostname, clientPort) = Socket.hostnameAndPort(from: address)!
             print("Received lenght \(bytesRead) from \(clientHostname):\(clientPort)")
+            
+            //accedemos a los bytes del buffer de recepcion
+            // var offset = 0
+            // let point = buffer.withUnsafeBytes {
+            //     $0.load(fromByteOffset: offset ,as: Point.self)
+            // }
+            // offset += MemoryLayout<Point>.size
+            // print("Point: \(point)")
+            // let number = buffer.withUnsafeBytes {
+            //     $0.load(fromByteOffset: offset, as: Int.self)
+            // }
+            // print("Number: \(number)")
+            
             let message = String(decoding: buffer, as: UTF8.self)
             print("Client Message: \(message)\n")
-            let reply = "Server: \(message)"
+            let reply = "Server reply: \(message)\n"
             try serverSocket.write(from: reply, to: address)//devuelve el mensaje al cliente
         }
         buffer.removeAll()
